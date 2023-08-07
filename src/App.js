f74329ff622ec391b1783fc73e534000761890ed
Riyad <Riyad9876@gmail.com>
2023-08-07 11:02:12 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-
-
+import { Route, Routes} from 'react-router-dom'
+import Main from './pages/Main'
+import Currencies from './pages/Currencies'
+import Price from './pages/Price'
 import './App.css';
 
 
@@ -7,7 +9,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>CryptoPrices</h1>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/currencies' element={<Currencies/>}/>
+        <Route path='/price' element={<Price/>}/>
+      </Routes>
     </div>
   );
 }
