dec0f0f55e7e41621a8a94f15a2392ce91ecc235
Riyad <Riyad9876@gmail.com>
2023-08-07 11:45:17 -0400

    url parameter for price. ie  price/btc

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Main/>}/>
         <Route path='/currencies' element={<Currencies/>}/>
-        <Route path='/price' element={<Price/>}/>
+        <Route path='/price/:symbol' element={<Price/>}/>
       </Routes>
     </div>
   );
